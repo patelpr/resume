5c4225c4e3ea152e188c8f1f9c98aea7c67c2f67
dev <devjutzu@gmail.com>
2020-06-16 08:16:07 -0400

    init

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+<div>
+	<Container />
+</div>
+</template>
+
+<script>
+import Container from './components/Container';
+
+export default {
+  name: 'App',
+  components: {
+    Container,
+  },
+};
+</script>
+<style>
+
+
+
+</style>
